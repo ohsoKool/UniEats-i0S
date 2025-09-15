2d1bda88184e865b024e001938ac0cb2f36ad7ca
ohsoKool <rishikeshgunda9@gmail.com>
2025-09-15 21:27:28 +0530

    Initial Commit

---
+++ UniEatsPage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UniEatsPage
+//
+//  Created by Rishikesh Gunda on 9/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UniEatsPage/UniEatsPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniEatsPageApp.swift
+//  UniEatsPage
+//
+//  Created by Rishikesh Gunda on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct UniEatsPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
